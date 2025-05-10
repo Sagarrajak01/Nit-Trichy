b329258bff58257b3829aad00c120966bad28d40
Sagar Rajak <65167758+Sagarrajak01@users.noreply.github.com>
2025-05-10 15:28:56 +0530

    Course-column

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ let navLinks = document.getElementById("navLinks");
 
 function showMenu() {
     navLinks.style.right = "0";
-    document.addEventListener("click",outsideClickHandler);
+    document.addEventListener("click", outsideClickHandler);
 }
 
 function hideMenu() {
     navLinks.style.right = "-200px";
-    document.removeEventListener("click",outsideClickHandler);
+    document.removeEventListener("click", outsideClickHandler);
 }
 
-function outsideClickHandler(e){
+function outsideClickHandler(e) {
     const isClickInside = navLinks.contains(e.target);
     const isMenuButton = e.target.classList.contains("fa-bars");
 
-    if(!isClickInside && !isMenuButton){
+    if (!isClickInside && !isMenuButton) {
         hideMenu();
     }
 }
