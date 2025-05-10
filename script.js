82da2410c2297ccb48abd6f6dd15f60e8821bb80
Sagar Rajak <65167758+Sagarrajak01@users.noreply.github.com>
2025-05-10 15:03:49 +0530

    Initial Body

---
+++ script.js
@@ -0,0 +1,20 @@
+let navLinks = document.getElementById("navLinks");
+
+function showMenu() {
+    navLinks.style.right = "0";
+    document.addEventListener("click",outsideClickHandler);
+}
+
+function hideMenu() {
+    navLinks.style.right = "-200px";
+    document.removeEventListener("click",outsideClickHandler);
+}
+
+function outsideClickHandler(e){
+    const isClickInside = navLinks.contains(e.target);
+    const isMenuButton = e.target.classList.contains("fa-bars");
+
+    if(!isClickInside && !isMenuButton){
+        hideMenu();
+    }
+}
